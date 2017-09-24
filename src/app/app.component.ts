1e8c3186f61cf78a24896bfd17003f773e234678
EliCDavis <eli.davis1995@gmail.com>
2017-09-24 06:27:00 -0500

    cute styling for subscribe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,8 @@ export class AppComponent implements OnInit {
   constructor(iconRegistry: MdIconRegistry, sanitizer: DomSanitizer, auth: AuthenticationService) {
     iconRegistry.addSvgIcon('add', sanitizer.bypassSecurityTrustResourceUrl('assets/ic_add_circle_outline_black_24px.svg'));
     iconRegistry.addSvgIcon('point', sanitizer.bypassSecurityTrustResourceUrl('assets/ic_location_on_black_24px.svg'));
+    iconRegistry.addSvgIcon('starred', sanitizer.bypassSecurityTrustResourceUrl('assets/ic_star_black_24px.svg'));
+    iconRegistry.addSvgIcon('unstarred', sanitizer.bypassSecurityTrustResourceUrl('assets/ic_star_border_black_24px.svg'));
   }
 
   ngOnInit() { }
